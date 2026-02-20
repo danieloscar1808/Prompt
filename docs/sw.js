094bcb2c89a3bf24183b9b4820987a5e0da4d797
danieloscar1808 <danieloscar1808@gmail.com>
2026-02-20 10:06:17 -0300

    docs

--- docs/sw.js
+++
@@ -1,6 +0,0 @@
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open("pm-cache-v1").then(cache => cache.addAll(["./"])));
-});
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
-});
